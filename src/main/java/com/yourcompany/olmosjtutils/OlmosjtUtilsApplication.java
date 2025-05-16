7c4790ee428be4a52f0b9dea2e963a09e4073565
olmosjt <ol.davronov@tengebank.uz>
2025-05-16 12:40:21 +0500

    Repository started.

---
+++ src/main/java/com/yourcompany/olmosjtutils/OlmosjtUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.yourcompany.olmosjtutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlmosjtUtilsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OlmosjtUtilsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/yourcompany/olmosjtutils/OlmosjtUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcompany.olmosjtutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlmosjtUtilsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

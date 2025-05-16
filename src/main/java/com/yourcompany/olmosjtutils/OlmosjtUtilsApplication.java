6a6a6b50d494d559f8b00f7808cd0a3c410859a5
olmosjt <ol.davronov@tengebank.uz>
2025-05-16 15:17:53 +0500

    removed boilerplate test code

--- src/main/java/com/yourcompany/olmosjtutils/OlmosjtUtilsApplication.java
+++ src/main/java/com/yourcompany/olmosjtutils/OlmosjtUtilsApplication.java
@@ -13,7 +13,7 @@ import java.util.Currency;
 
 @Slf4j
 @SpringBootApplication
-public class OlmosjtUtilsApplication implements ApplicationRunner {
+public class OlmosjtUtilsApplication {
 
   public static void main(String[] args) {
     var application = new SpringApplication(OlmosjtUtilsApplication.class);
@@ -21,50 +21,5 @@ public class OlmosjtUtilsApplication implements ApplicationRunner {
     ApplicationUtils.logApplicationStartup(env);
   }
 
-  @Override
-  public void run(ApplicationArguments args) throws Exception {
-      log.info(new DTO(
-              "51702026bnmbnm670039",
-              "olmos@jt20@gmail.com",
-              "+998 (93) 008-24-17",
-              "AB",
-              "9319901",
-              new BigDecimal("3900000"),
-              "Internal Note"
-      ).toString());
-  }
 
-  record DTO(
-          String userPinfl,
-          String userEmail,
-          String UserPhone,
-          String UsrPassportSeries,
-          String UsrPassportNumber,
-          BigDecimal amount,
-          String internalNote
-  ) {
-    @Override
-    public String toString() {
-      return """
-          DTO[
-           userPinfl: %s,
-           userEmail: %s,
-           UserPhone: %s,
-           UsrPassportSeries: %s,
-           UsrPassportNumber: %s,
-           Amount: %s,
-           internalNote: %s
-          ]
-          """.formatted(
-              MaskingUtils.maskPinfl(userPinfl),
-              MaskingUtils.maskEmail(userEmail),
-              MaskingUtils.maskPhoneNumber(UserPhone),
-              MaskingUtils.maskPassportSeries(UsrPassportSeries),
-              MaskingUtils.maskPassportNumber(UsrPassportNumber),
-              MaskingUtils.maskAmountWithCurrency(amount, "UZS"),
-              this.internalNote
-      );
-
-    }
-  }
 }
